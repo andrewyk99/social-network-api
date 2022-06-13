a5f335eb12d3932fa9e32219a3ae0654a6b6c79c
andrewyk99 <andrewyk99@hotmail.com>
2022-06-12 23:03:46 -0600

    added date formatter

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const ReactionSchema = new Schema(
     {
@@ -20,7 +21,7 @@ const ReactionSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
-            // Use getter method to format timestamp
+            get: createdAtVal => dateFormat(createdAtVal)
         }
     },
     {
@@ -41,8 +42,8 @@ const ThoughtSchema = new Schema(
         },
         createdAt: {
             type: Date,
-            default: Date.now
-            // Need to use a get to format timestamp
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
         },
         username: {
             type: String,
