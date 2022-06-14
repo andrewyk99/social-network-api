033d412acd4231a691b294e06df4d8a63637ef38
Andrew kim <andrewyk99@hotmail.com>
2022-06-13 22:03:09 -0600

    fixed user validators

--- models/User.js
+++ models/User.js
@@ -4,14 +4,14 @@ const UserSchema = new Schema(
     {
         username: {
             type: String,
-            Unique: true,
-            Required: 'You need to make a USERNAME!',
+            unique: true,
+            required: true,
             trim: true
         },
         email: {
             type: String,
-            Unique: true,
-            Required: 'You need to type in your email!',
+            unique: true,
+            required: 'You need to type in your email!',
             match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address!']
         },
         thoughts: [
